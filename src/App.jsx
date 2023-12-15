0170307723fe6b5f38e71d59fa6f6b218f2e2991
Anand <anandjayara@gmail.com>
2023-12-15 01:09:59 +0000

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,49 @@
-import "./App.css";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { checkAuthAsync, selectLoggedInUser, selectisAuthChecked } from "./features/auth/AuthSlice";
-import { BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import { Cart, Home, Login, Menu, Newproduct, Signup } from "./pages";
 import { Protected } from "./features/auth/components/Protected";
 import { getCartItemsByUserIdAsync } from "./features/cart/CartSlice";
 
-
 function App() {
-
   const dispatch = useDispatch();
   const user = useSelector(selectLoggedInUser);
   const userChecked = useSelector(selectisAuthChecked);
 
   useEffect(() => {
+    // Effect to check authentication status
     dispatch(checkAuthAsync());
   }, [dispatch]);
 
-  useEffect(()=>{
-    if(user){
-      dispatch(getCartItemsByUserIdAsync(user._id))
+  useEffect(() => {
+    // Effect to fetch cart items when user is available
+    if (user) {
+      dispatch(getCartItemsByUserIdAsync(user._id));
     }
-  },[user])
+  }, [user, dispatch]);
 
   useEffect(() => {
-    if(userChecked){
+    // Effect for additional logic when userChecked changes
+    if (userChecked) {
+      // Your code here if needed
     }
   }, [userChecked]);
 
+  // JSX for the component structure
   return (
     <Router>
-        {
-          userChecked && <Routes>
-           <Route exact path='/' element={<Protected><Home /></Protected>} />
-          <Route exact path="/menu" element={<Protected><Menu /></Protected>} />
-          <Route exact  path="/menu/:filterby" element={<Protected><Menu /></Protected>} />
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/signup" element={<Signup />} />
-          <Route exact path="/newproduct" element={<Protected><Newproduct /></Protected>} />
-          <Route exact path="/cart" element={<Protected><Cart/></Protected>} />
-          </Routes>
-        }
-         
+      {userChecked && (
+        <Routes>
+          <Route path='/' element={<Protected><Home /></Protected>} />
+          <Route path="/menu" element={<Protected><Menu /></Protected>} />
+          <Route path="/menu/:filterby" element={<Protected><Menu /></Protected>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/newproduct" element={<Protected><Newproduct /></Protected>} />
+          <Route path="/cart" element={<Protected><Cart /></Protected>} />
+        </Routes>
+      )}
     </Router>
   );
 }
